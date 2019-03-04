645a60946c7bacc06710c3aabc83678f6d0c8c12
MarDatsko <datskomar@gmail.com>
2019-03-04 11:30:27 +0200

    start mission 1

--- pllug.cpp
+++ pllug.cpp
@@ -2,7 +2,23 @@
 
 int main()
 {
-std::cout<<"Hrllo GitHub";
-std::cout<<"Hrllo GitHub";
-std::cout<<"Hrllo GitHub";
+std::string choice_user;
+std::string choise_comp;
+int a;
+std::cout<<"Enter Rock,Scissors,Paper";
+std::cin>>choice;
+a = rand()%3;
+if (string choise_user == rock || string choise_user == Rock)
+{
+choise_user = 0;
+}
+if (string choise_user == scissors || string choise_user == Scissors)
+{
+choise_user = 1;
+}
+if (string choise_user == paper || string choise_user == Paper) 
+{ 
+choise_user = 2;
+}
+
 }
